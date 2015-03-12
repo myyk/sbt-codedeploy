6ce65747407cf85b9dccaa4ef4893971f7b16db7
Myyk Seok <mseok@gilt.com>
2015-03-11 18:52:43 -0700

    Fix simple-example scripts so that the project starts up and shuts down correctly.

--- examples/simple-example/src/codedeploy/scripts/ApplicationStart/01start.sh
+++ examples/simple-example/src/codedeploy/scripts/ApplicationStart/01start.sh
@@ -1,1 +1,3 @@
-nohup java ExampleMain &
+#!/bin/bash
+
+java -cp /opt/gilt/svc-example/lib/scala-library.jar:/opt/gilt/svc-example/lib/simpleexample_2.10-0.1-SNAPSHOT.jar ExampleMain &
--- examples/simple-example/src/codedeploy/scripts/ApplicationStop/01stop.sh
+++ examples/simple-example/src/codedeploy/scripts/ApplicationStop/01stop.sh
@@ -1,1 +1,7 @@
-pkill -f ExampleMain
+#!/bin/bash
+
+if pkill -f ExampleMain; then
+    echo "Shut down ExampleMain"
+else
+    echo "ExampleMain was not running"
+fi
