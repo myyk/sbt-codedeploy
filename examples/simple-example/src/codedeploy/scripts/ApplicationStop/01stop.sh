6cf024cfc3ed83ac865c5eb28c1190ff7e95d59e
Myyk Seok <mseok@gilt.com>
2015-03-12 18:33:12 -0700

    Try to fix simple-example. Still needs work on startup.

--- examples/simple-example/src/codedeploy/scripts/ApplicationStop/01stop.sh
+++ examples/simple-example/src/codedeploy/scripts/ApplicationStop/01stop.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-if pkill -f ExampleMain; then
+PID="$(pgrep -f ExampleMain)"
+while sleep 0.2
+      echo Testing again
+      kill $PID >/dev/null 2>&1
+do
+    # Code to kill process
     echo "Shut down ExampleMain"
-else
-    echo "ExampleMain was not running"
-fi
+done
